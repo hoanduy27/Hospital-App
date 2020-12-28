555e0126f876e38f028b8554463b72d520e44180
hoanduy27 <50733059+hoanduy27@users.noreply.github.com>
2020-12-28 21:12:07 +0700

    Update BacSi_8.sql

--- SQL/BacSi/BacSi_8.sql
+++ SQL/BacSi/BacSi_8.sql
@@ -1,4 +1,4 @@
-﻿USE HospitalDB1
+USE HospitalDB1
 GO
 
 --8. Xem các bệnh nhân của cùng 1 bệnh mà mình đã chẩn đoán
@@ -7,41 +7,55 @@ CREATE OR ALTER PROCEDURE PROC_8
 	@ten_benh varchar(100)
 AS
 BEGIN
-	SELECT DISTINCT BenhNhan.*
+	--Chan doan XN tu bac si chinh
+	SELECT DISTINCT BenhNhan.CMND, BenhNhan.Ho, BenhNhan.Ten
 	FROM  BenhNhan INNER JOIN(
-			(CuocKham INNER JOIN (CoXetNghiem INNER JOIN XetNghiem ON CoXetNghiem.MaXN = XetNghiem.MaXN AND XetNghiem.TenXN = @ten_benh) 
-			ON CuocKham.MaKhamBenh = CoXetNghiem.MaKhamBenh AND CuocKham.MaBacSi = @ma_bac_si) )
-		ON BenhNhan.CMND = CuocKham.MaBN
+			(CuocKham INNER JOIN 
+				(CoXetNghiem INNER JOIN XetNghiem 
+				ON 
+					CoXetNghiem.MaXN = XetNghiem.MaXN 
+					AND XetNghiem.TenXN = @ten_benh) 
+			ON 
+				CuocKham.MaKhamBenh = CoXetNghiem.MaKhamBenh 
+				AND CuocKham.MaBacSi = @ma_bac_si) )
+	ON BenhNhan.CMND = CuocKham.MaBN
 
 	UNION
-
-	SELECT DISTINCT BenhNhan.*
+	--Chan doan chup phim tu BS chinh
+	SELECT DISTINCT BenhNhan.CMND, BenhNhan.Ho, BenhNhan.Ten
 	FROM  BenhNhan INNER JOIN(
 			(CuocKham INNER JOIN CoChupPhim 
-			ON CuocKham.MaKhamBenh = CoChupPhim.MaKhamBenh AND CoChupPhim.KetQua = @ten_benh 
-															AND CuocKham.MaBacSi = @ma_bac_si) )
+			ON 
+				CuocKham.MaKhamBenh = CoChupPhim.MaKhamBenh 
+				AND CoChupPhim.KetQua = @ten_benh 
+				AND CuocKham.MaBacSi = @ma_bac_si) )
 	ON BenhNhan.CMND = CuocKham.MaBN
 
 	UNION 
-
-	SELECT DISTINCT BenhNhan.*
+	--Chan doan XN tu BSXN
+	SELECT DISTINCT BenhNhan.CMND, BenhNhan.Ho, BenhNhan.Ten
 	FROM  BenhNhan INNER JOIN(
-			(CuocKham INNER JOIN (CoXetNghiem INNER JOIN XetNghiem ON CoXetNghiem.MaXN = XetNghiem.MaXN AND XetNghiem.TenXN = @ten_benh) 
-			ON CuocKham.MaKhamBenh = CoXetNghiem.MaKhamBenh AND CoXetNghiem.MaBSXN = @ma_bac_si) )
-		ON BenhNhan.CMND = CuocKham.MaBN
+			(CuocKham INNER JOIN 
+				(CoXetNghiem INNER JOIN XetNghiem 
+				ON 
+					CoXetNghiem.MaXN = XetNghiem.MaXN 
+					AND XetNghiem.TenXN = @ten_benh) 
+			ON 
+				CuocKham.MaKhamBenh = CoXetNghiem.MaKhamBenh 
+				AND CoXetNghiem.MaBSXN = @ma_bac_si) )
+	ON BenhNhan.CMND = CuocKham.MaBN
 
 	UNION
-
-	SELECT DISTINCT BenhNhan.*
+	--Chan doan chup phim tu BSCP
+	SELECT DISTINCT BenhNhan.CMND, BenhNhan.Ho, BenhNhan.Ten
 	FROM  BenhNhan INNER JOIN(
 			(CuocKham INNER JOIN CoChupPhim 
-			ON CuocKham.MaKhamBenh = CoChupPhim.MaKhamBenh AND CoChupPhim.KetQua = @ten_benh 
-															AND CoChupPhim.MaBSCP = @ma_bac_si) )
+			ON 
+				CuocKham.MaKhamBenh = CoChupPhim.MaKhamBenh 
+				AND CoChupPhim.KetQua = @ten_benh 
+				AND CoChupPhim.MaBSCP = @ma_bac_si) )
 	ON BenhNhan.CMND = CuocKham.MaBN
 END
 GO
 
-
-
-EXEC PROC_8 100000027,'U lanh o nao'
-GO
+EXEC PROC_8 '100000011', 'Tieu Duong'
