a5347be8e332731b77a56d6041495b85fc405f95
hoanduy27 <50733059+hoanduy27@users.noreply.github.com>
2020-12-28 20:57:56 +0700

    Update BacSi_4.sql

--- SQL/BacSi/BacSi_4.sql
+++ SQL/BacSi/BacSi_4.sql
@@ -1,4 +1,4 @@
-﻿USE HospitalDB1
+USE HospitalDB1
 GO
 
 --4. Xem các chẩn đóan bệnh của bệnh nhân mà mình đã phụ trách
@@ -7,44 +7,60 @@ CREATE OR ALTER PROCEDURE PROC_4
 	@ma_benh_nhan nchar(9)
 AS
 BEGIN
-	(SELECT DISTINCT XetNghiem.TenXN AS BenhChanDoan
-	FROM XetNghiem INNER JOIN (CuocKham INNER JOIN CoXetNghiem ON CuocKham.MaBN = @ma_benh_nhan AND CuocKham.MaBacSi = @ma_bac_si AND CuocKham.MaKhamBenh = CoXetNghiem.MaKhamBenh) 
+	(SELECT DISTINCT 
+		CuocKham.ThoiGianKham AS ThoiGian, 
+		'XN: ' + XetNghiem.TenXN AS TenXN_CP, 
+		CoXetNghiem.ChanDoanXetNghiem AS ChanDoan
+	FROM XetNghiem INNER JOIN 
+		(CuocKham INNER JOIN CoXetNghiem 
+		ON 
+			CuocKham.MaBN = @ma_benh_nhan 
+			AND CuocKham.MaBacSi = @ma_bac_si 
+			AND CuocKham.MaKhamBenh = CoXetNghiem.MaKhamBenh) 
 	ON XetNghiem.MaXN = CoXetNghiem.MaXN)
 
 	UNION
 
-	(SELECT DISTINCT CoChupPhim.KetQua AS BenhChanDoan
-	FROM CuocKham INNER JOIN CoChupPhim 
-	ON CuocKham.MaBN = @ma_benh_nhan AND CuocKham.MaBacSi = @ma_bac_si AND CuocKham.MaKhamBenh = CoChupPhim.MaKhamBenh)
+	(SELECT DISTINCT 
+		CuocKham.ThoiGianKham AS ThoiGian, 
+		'CP: ' + CoChupPhim.TenChupPhim AS TenXN_CP, 
+		CoChupPhim.KetQua AS ChanDoan
+	FROM CuocKham INNER JOIN CoChupPhim
+	ON
+		CuocKham.MaBN = @ma_benh_nhan
+		AND CuocKham.MaBacSi = @ma_bac_si
+		AND CuocKham.MaKhamBenh = CoChupPhim.MaKhamBenh)
 
 	UNION
 
-	(SELECT DISTINCT XetNghiem.TenXN AS BenhChanDoan
-	FROM XetNghiem INNER JOIN (CuocKham INNER JOIN CoXetNghiem ON CuocKham.MaBN = @ma_benh_nhan AND CoXetNghiem.MaBSXN = @ma_bac_si AND CuocKham.MaKhamBenh = CoXetNghiem.MaKhamBenh) 
+	(SELECT DISTINCT 
+		CuocKham.ThoiGianKham AS ThoiGian, 
+		'XN: ' + XetNghiem.TenXN AS TenXN_CP, 
+		CoXetNghiem.ChanDoanXetNghiem AS ChanDoan
+	FROM XetNghiem INNER JOIN 
+		(CuocKham INNER JOIN CoXetNghiem 
+		ON 
+			CuocKham.MaBN = @ma_benh_nhan 
+			AND CoXetNghiem.MaBSXN = @ma_bac_si 
+			AND CuocKham.MaKhamBenh = CoXetNghiem.MaKhamBenh) 
 	ON XetNghiem.MaXN = CoXetNghiem.MaXN)
 
 	UNION
 
-	(SELECT DISTINCT CoChupPhim.KetQua AS BenhChanDoan
-	FROM CuocKham INNER JOIN CoChupPhim 
-	ON CuocKham.MaBN = @ma_benh_nhan AND CoChupPhim.MaBSCP = @ma_bac_si AND CuocKham.MaKhamBenh = CoChupPhim.MaKhamBenh)
-
-	--UNION
-
-	--(SELECT DISTINCT BenhNhapVien.TenBenh AS BenhChanDoan
-	--FROM BenhNhapVien INNER JOIN BenhNhanNoiTru
-	--ON BenhNhanNoiTru.CMND = BenhNhapVien.MaBN AND BenhNhanNoiTru.CMND = @ma_benh_nhan AND BenhNhanNoiTru.MaBSChiDinhNV = @ma_bac_si)
-
-	--UNION
-
-	--(SELECT DISTINCT BenhXuatVien.TenBenh AS BenhChanDoan
-	--FROM BenhXuatVien INNER JOIN BenhNhanNoiTru
-	--ON BenhNhanNoiTru.CMND = BenhXuatVien.MaBN AND BenhNhanNoiTru.CMND = @ma_benh_nhan AND BenhNhanNoiTru.MaBSChiDinhXV = @ma_bac_si)
+	(SELECT DISTINCT 
+		CuocKham.ThoiGianKham AS ThoiGian, 
+		'CP: ' + CoChupPhim.TenChupPhim AS TenXN_CP, 
+		CoChupPhim.KetQua AS ChanDoan
+	FROM CuocKham INNER JOIN CoChupPhim
+	ON
+		CuocKham.MaBN = @ma_benh_nhan
+		AND CoChupPhim.MaBSCP = @ma_bac_si
+		AND CuocKham.MaKhamBenh = CoChupPhim.MaKhamBenh)
 												
 END
 GO
 
-
-
-EXEC PROC_4 100000005,225600012
-GO
+EXEC PROC_4 '100000006', '225600001'
+EXEC PROC_4 '100000011', '225600001'
+EXEC PROC_4 '100000001', '225600003'
+EXEC PROC_4 '100000027', '225600003'
