a0e4b883b913cc5be4f1ab18b8735dbef3785548
hoanduy27 <50733059+hoanduy27@users.noreply.github.com>
2020-12-28 21:15:59 +0700

    Update BacSi_10.sql

--- SQL/BacSi/BacSi_10.sql
+++ SQL/BacSi/BacSi_10.sql
@@ -1,4 +1,4 @@
-﻿USE HospitalDB1
+USE HospitalDB1
 GO
 
 --10. Xem các bệnh nhân đã xuất viện mà mình đã phụ trách 
@@ -6,12 +6,17 @@ CREATE OR ALTER PROCEDURE PROC_10
 	@ma_bac_si nchar(9)
 AS
 BEGIN
-	SELECT DISTINCT BenhNhan.* 
-	FROM BenhNhan INNER JOIN BenhAnNoiTru
-	ON BenhAnNoiTru.MaBSChiDinhXV = @ma_bac_si AND BenhNhan.CMND = BenhAnNoiTru.MaBN ;
+	SELECT t.CMND, Ho, Ten
+	FROM
+		(SELECT DISTINCT CMND
+		FROM BenhNhanNoiTru INNER JOIN BenhAnNoiTru
+		ON 
+			BenhAnNoiTru.MaBN = BenhNhanNoiTru.CMND
+			AND BenhAnNoiTru.MaBSChiDinhXV = @ma_bac_si
+		) t,
+		BenhNhan
+	WHERE t.CMND = BenhNhan.CMND;
+
 END
 GO
-
-
-EXEC PROC_10 100000007
-GO
+EXEC PROC_10 '100000005'
