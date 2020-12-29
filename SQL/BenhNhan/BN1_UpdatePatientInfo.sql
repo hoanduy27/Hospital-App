d2ddc88dfe030a9877845859f3732fe24e179af7
hoanduy27 <50733059+hoanduy27@users.noreply.github.com>
2020-12-29 20:49:52 +0700

    Update BN1_UpdatePatientInfo.sql

--- SQL/BenhNhan/BN1_UpdatePatientInfo.sql
+++ SQL/BenhNhan/BN1_UpdatePatientInfo.sql
@@ -3,13 +3,35 @@ GO
 --1: Cap nhat thong tin nhan khau hoc va bhyt
 create or alter proc BN1_UpdatePatientInfo
 	@cmnd nchar(9),
-	@bhyt nchar(15),
+	@bhyt varchar(15),
 	@GioiTinh nchar(1),
-	@ChieuCao int,
-	@CanNang int
+	@ChieuCao INTEGER,
+	@CanNang INTEGER
 as
 begin
+	DECLARE @oldBHYT NCHAR(15);
+	DECLARE @oldGioiTinh NCHAR(1);
+	DECLARE @oldChieuCao INTEGER;
+	DECLARE @oldCanNang INTEGER;
+
+	SELECT @oldBHYT = MaBHYT, @oldGioiTinh = GioiTinh, @oldChieuCao = ChieuCao, @oldCanNang = CanNang
+	FROM BenhNhan
+	WHERE CMND = @cmnd
+
+	IF(@bhyt = '')
+		SET @bhyt = @oldBHYT;
+	IF(@GioiTinh = '')
+		SET @GioiTinh = @oldGioiTinh;
+	IF(@ChieuCao = 0)
+		SET @ChieuCao = @oldChieuCao
+	IF(@CanNang = 0)
+		SET @CanNang = @oldCanNang
+
 	update BenhNhan
-	set MaBHYT=@bhyt, GioiTinh = @GioiTinh, ChieuCao = @ChieuCao, CanNang=@CanNang
+	set MaBHYT=@bhyt, GioiTinh = @GioiTinh, ChieuCao = @ChieuCao, CanNang= @CanNang
 	where CMND = @cmnd
 end
+GO
+
+
+
